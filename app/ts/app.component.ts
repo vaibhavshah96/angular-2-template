e66f739b9dcbd3761ece0b47ce23a6b06751d054
vaibhavshah96 <vaibhavshah96@gmail.com>
2017-09-10 11:41:06 +0530

    commit 2
    playlist

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -1,8 +1,22 @@
 import {Component} from 'angular2/core';
+import {Config} from './config.service';
+import {Video} from './video';
+import {PlaylistComponent} from './playlist.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    templateUrl: 'app/ts/app.component.html',
+    directives : [PlaylistComponent]
 })
 
-export class AppComponent {}
+export class AppComponent {
+	mainHeading = Config.MAIN_HEADING;
+	videos:Array<Video>;
+
+	constructor(){
+		this.videos = [
+			new Video(1, "abc", "iPsPGqWKY9U", "lol"),
+			new Video(2, "xyz", "v7QsVe28xyE", "looool")
+		]
+	}
+}
---
+++ app/ts/video.ts
@@ -0,0 +1,15 @@
+export class Video {
+	id:number;
+	title:string;
+	videoCode:atring;
+	desc:string;
+
+	constructor(id:number, title:string, videoCode:string, desc:string){
+		this.id = id;
+		this.title = title;
+		this.videoCode = videoCode;
+		this.desc = desc;
+
+	}
+
+}
